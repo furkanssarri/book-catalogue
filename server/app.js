73bdf2b781cef5a313a9c9725758c3d67cd9168f
Furkan Sarı <furkansa@hotmail.com.tr>
2025-09-30 00:23:35 +0300

    feat: add basic routing & setup view engine

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const path = require("node:path");
+
+const booksRouter = require("./routes/booksRouter.js");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (_req, res) => {
-  res.send("hello world!");
+  res.render("index", { title: "Home" });
 });
+
+app.use("/books", booksRouter);
 
 app.listen(PORT, (err) => {
   if (err) {
