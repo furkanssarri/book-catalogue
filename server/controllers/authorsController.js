9a87e2c6c12c90eec441ed9651f5a6e9687a29ea
Furkan Sarı <furkansa@hotmail.com.tr>
2025-10-03 16:47:50 +0300

    feat: add /new /edit /delete & controllers
    all CRUD operations for authors should now be fully functional. the
    delete logic checks if the author has any books associated with them and
    prevents deletion if so. otherwise, the author is deleted normally.
    
    also, this commit refactors the router to be more RESTFUL i suppose.

--- server/controllers/authorsController.js
+++ server/controllers/authorsController.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+const validateAuthor = require("../validators/validateAuthor.js");
 const db = require("./queryDb.js");
 
 exports.allAuthorsGet = async (_req, res) => {
@@ -11,7 +13,7 @@ exports.allAuthorsGet = async (_req, res) => {
 
 exports.singleAuthorGet = async (req, res) => {
   try {
-    const authorId = parseInt(req.params.authorId);
+    const authorId = parseInt(req.params.authorId, 10);
     const author = await db.getSingleAuthor(authorId);
     res.render("pages/authorDetails", {
       title: "Author Details",
@@ -19,5 +21,91 @@ exports.singleAuthorGet = async (req, res) => {
     });
   } catch (err) {
     console.error(err);
+  }
+};
+
+exports.addNewAuthorGet = async (req, res) => {
+  try {
+    res.render("pages/authorForm", {
+      title: "Add New Author",
+      method: "POST",
+      action: `/authors/new`,
+      errors: null,
+      old: {},
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+exports.addNewAuthorPost = [
+  validateAuthor,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render("pages/authorForm", {
+          title: "Add New Author",
+          method: "POST",
+          action: "/authors/new",
+          errors: errors.mapped(),
+          old: req.body,
+        });
+      }
+      const newAuthor = await db.addNewAuthor(req.body);
+    } catch (err) {
+      console.error(err);
+    }
+    res.redirect("/authors");
+  },
+];
+
+exports.editAuthorGet = async (req, res) => {
+  try {
+    const authorId = parseInt(req.params.authorId, 10);
+    const author = await db.getSingleAuthor(authorId);
+    res.render("pages/authorForm", {
+      title: "Edit Author",
+      method: "POST",
+      action: `/authors/${req.params.authorId}/edit?_method=PUT`,
+      errors: null,
+      old: author,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+exports.editAuthorPost = async (req, res) => {
+  try {
+    const authorId = parseInt(req.params.authorId, 10);
+    const { name, country, bio } = req.body;
+    const newAuthor = {
+      name,
+      country,
+      bio,
+    };
+    await db.updateAuthor(authorId, newAuthor);
+  } catch (err) {
+    console.error(err);
+  }
+  res.redirect("/authors");
+};
+
+exports.deleteAuthor = async (req, res) => {
+  try {
+    const authorId = parseInt(req.params.authorId, 10);
+    const booksByAuthor = await db.getBooksByAuthor(authorId);
+
+    if (booksByAuthor.length > 0) {
+      console.error("Cannot delete author with linked books.");
+      return res.status(400).redirect("/authors");
+    }
+
+    await db.deleteAuthor(authorId);
+    res.redirect("/authors");
+  } catch (err) {
+    console.error("Error deleting author:", err);
+    res.status(500).send("Server error deleting author");
   }
 };
--- server/routes/authorsRouter.js
+++ server/routes/authorsRouter.js
@@ -1,9 +1,28 @@
 const { Router } = require("express");
-const authorsController = require("../controllers/authorsController.js");
+const authorsController = require("../controllers/authorsController");
+const validateAuthor = require("../validators/validateAuthor.js");
 
 const authorsRouter = Router();
 
+// list all authors
 authorsRouter.get("/", authorsController.allAuthorsGet);
+
+// new author form + create
+authorsRouter.get("/new", authorsController.addNewAuthorGet);
+authorsRouter.post("/new", validateAuthor, authorsController.addNewAuthorPost);
+
+// edit author form + update
+authorsRouter.get("/:authorId/edit", authorsController.editAuthorGet);
+authorsRouter.post(
+  "/:authorId",
+  validateAuthor,
+  authorsController.editAuthorPost,
+);
+
+// single author details
 authorsRouter.get("/:authorId", authorsController.singleAuthorGet);
+
+// delete author
+authorsRouter.post("/:authorId/delete", authorsController.deleteAuthor);
 
 module.exports = authorsRouter;
