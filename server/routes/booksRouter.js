0b5891c55ed9d9b9993ed952702c3d27d2cf5553
Furkan Sarı <furkansa@hotmail.com.tr>
2025-10-03 16:53:18 +0300

    feat: add edit & delete books functionalities

--- server/routes/booksRouter.js
+++ server/routes/booksRouter.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
+const booksController = require("../controllers/booksController.js");
 const booksRouter = Router();
 
-const booksController = require("../controllers/booksController.js");
-
 booksRouter.get("/", booksController.allBooksGet);
+
+booksRouter.get("/:bookId/edit", booksController.editBookGet);
+booksRouter.post("/:bookId/edit", booksController.editBookPost);
+
 booksRouter.get("/:bookId", booksController.singleBookGet);
+
+booksRouter.post("/:bookId/delete", booksController.deleteBookPost);
 
 module.exports = booksRouter;
--- server/routes/newBookRouter.js
+++ server/routes/newBookRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
+const validateBook = require("../validators/bookValidator.js");
 const newBookController = require("../controllers/newBookController.js");
 
 const newbookRouter = Router();
 
 newbookRouter.get("/", newBookController.newBookFormGet);
-newbookRouter.post("/", newBookController.newBookPost);
+newbookRouter.post("/", validateBook, newBookController.newBookPost);
 
 module.exports = newbookRouter;
