cec05cd549072e1c98f0a01ce54fe628d9dc6fd3
Furkan Sarı <furkansa@hotmail.com.tr>
2025-10-03 16:56:47 +0300

    add controller logic that was forgotten in the previous commit

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,6 +1,6 @@
 const db = require("./queryDb.js");
 
-exports.allBooksGet = async (req, res) => {
+exports.allBooksGet = async (_req, res) => {
   try {
     const books = await db.getAllBooks();
     res.render("pages/books", { title: "Books", books: books });
@@ -11,7 +11,7 @@ exports.allBooksGet = async (req, res) => {
 
 exports.singleBookGet = async (req, res) => {
   try {
-    const bookId = parseInt(req.params.bookId);
+    const bookId = parseInt(req.params.bookId, 10);
     const book = await db.getBookById(bookId);
     res.render("pages/bookDetails", {
       title: "Book Details",
@@ -22,13 +22,57 @@ exports.singleBookGet = async (req, res) => {
   }
 };
 
-exports.latestBooksGet = async (req, res) => {
+exports.latestBooksGet = async (_req, res) => {
   try {
     const latestBooks = await db.getLatestBooks(8);
     res.render("pages/index", {
       title: "Home",
       latestBooks: latestBooks,
     });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+exports.editBookGet = async (req, res) => {
+  try {
+    const bookId = parseInt(req.params.bookId, 10);
+    const book = await db.getBookById(bookId);
+    res.render("pages/bookForm", {
+      title: "Edit Book",
+      method: "POST",
+      action: `/books/${bookId}/edit`,
+      errors: null,
+      old: book,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+exports.editBookPost = async (req, res) => {
+  try {
+    const bookId = parseInt(req.params.bookId, 10);
+    const { title, description, publishDate, isbn } = req.body;
+    const newBook = {
+      title,
+      description,
+      publishDate,
+      isbn,
+    };
+    await db.updateBook(bookId, newBook);
+    console.log(newBook);
+  } catch (err) {
+    console.error(err);
+  }
+  res.redirect("/books");
+};
+
+exports.deleteBookPost = async (req, res) => {
+  try {
+    const bookId = parseInt(req.params.bookId, 10);
+    await db.deleteBook(bookId);
+    res.redirect("/books");
   } catch (err) {
     console.error(err);
   }
